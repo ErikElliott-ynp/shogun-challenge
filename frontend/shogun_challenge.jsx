319302a5c9451327e87c940d6af0fe000285648b
erikelliott-ynp <erikelliott92@gmail.com>
2021-03-15 17:41:26 -0700

    Finish project setup

---
+++ frontend/shogun_challenge.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const rootEl = document.getElementById("root");
+    ReactDom.render(<h1>Hi</h1>, rootEl)
+})
+
